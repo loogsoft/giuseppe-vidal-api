da6c727fe84c5eade035dea5141cb10176537846
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-03-12 23:43:30 -0300

    primeiro commit

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,12 +5,7 @@ export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: Number(configService.get('DB_PORT')),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_DATABASE'),
-  schema: configService.get<string>('DB_SCHEMA') ?? 'public',
+  url: configService.get<string>('DATABASE_URL'),
 
   ssl: {
     rejectUnauthorized: false,
